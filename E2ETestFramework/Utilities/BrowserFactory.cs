f66c72fe924d0d807339c0d288de784219f0627e
vkaneria <vkaneria@gleason.com>
2025-08-21 15:46:01 +0530

    centralized the logic for creating and configuring a Selenium WebDriver instance

---
+++ E2ETestFramework/Utilities/BrowserFactory.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace E2ETestFramework.Utilities
+{
+    public static class BrowserFactory
+    {
+        public static IWebDriver CreateDriver(TestSettings testSettings)
+        {
+            switch (testSettings.BrowserType.ToLower())
+            {
+                case "chrome":
+                    var chromeOptions = new ChromeOptions();
+                    if (testSettings.IsHeadless)
+                    {
+                        chromeOptions.AddArgument("--headless");
+                    }
+                    return new ChromeDriver(chromeOptions);
+                case "firefox":
+                    var firefoxOptions = new FirefoxOptions();
+                    if (testSettings.IsHeadless)
+                    {
+                        firefoxOptions.AddArgument("--headless");
+                    }
+                    return new FirefoxDriver(firefoxOptions);
+                case "edge":
+                    var edgeOptions = new EdgeOptions();
+                    if (testSettings.IsHeadless)
+                    {
+                        edgeOptions.AddArgument("--headless");
+                    }
+                    return new EdgeDriver(edgeOptions);
+                default:
+                    throw new NotSupportedException($"Browser '{testSettings.BrowserType}' is not supported.");
+            }
+        }
+    }
+}
