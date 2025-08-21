40af398ea80268b4b972df461fbcb7eb79c760f6
vkaneria <vkaneria@gleason.com>
2025-08-21 20:10:43 +0530

    added login page object class

---
+++ E2ETestFramework/Pages/LoginPage.cs
@@ -0,0 +1,22 @@
+﻿using E2ETestFramework.Utilities;
+using OpenQA.Selenium;
+
+namespace E2ETestFramework.Pages
+{
+    public class LoginPage : BasePage
+    {
+        // The constructor accepts the IWebDriver from the DI container and passes it to the BasePage.
+        public LoginPage(IWebDriver driver) : base(driver) { }
+
+        private readonly By _usernameField = By.CssSelector("input[id='username']");
+        private readonly By _passwordField = By.CssSelector("input[id='password']");
+        private readonly By _loginButton = By.CssSelector("button[type='submit']");
+
+        public void LoginAs(string username, string password)
+        {
+            Wait.SetValue(_usernameField, username);
+            Wait.SetValue(_passwordField, password);
+            Wait.Click(_loginButton);
+        }
+    }
+}
