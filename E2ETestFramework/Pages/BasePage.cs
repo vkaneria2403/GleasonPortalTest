a55c0d3c175657cdfe2635762df065e2bae05e9e
vkaneria <vkaneria@gleason.com>
2025-08-21 19:29:20 +0530

    refactor(pom): Introduce BasePage to reduce boilerplate code
    This commit adds an abstract `BasePage` class that provides a common
    constructor and a protected `IWebDriver` property.
    
    All concrete page objects will inherit from this class. This change
    removes duplicated DI constructor logic from each page object,
    adhering to the DRY (Don't Repeat Yourself) principle and simplifying
    the creation of new pages.

---
+++ E2ETestFramework/Pages/BasePage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace E2ETestFramework.Pages
+{
+    public abstract class BasePage
+    {
+        protected readonly IWebDriver Driver;
+        protected readonly WebDriverWait Wait;
+
+        protected BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+            Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+        }
+    }
+}
